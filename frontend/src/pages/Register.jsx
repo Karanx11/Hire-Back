1078d849595dc330aac07dd3de10b4002c32eaf4
Karan Sharma <karanx11.72898@gmail.com>
2026-05-16 01:26:59 +0530

    Fiing Ui

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -91,7 +91,7 @@ export default function Login() {
       >
 
         <h2 className="text-2xl font-bold text-center text-gray-800 mb-6">
-          Welcome Back !
+          Welcome Back !!
         </h2>
 
         {/* Role Toggle */}
